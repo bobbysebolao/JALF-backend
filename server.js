e856d709b98f2f9653fe1a98a5ed6c4c6218b072
Jessica Deacon <j.r.deacon@hotmail.co.uk>
2020-10-06 15:33:34 +0100

    set up procfile

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const db = require('./db/connection')
-const userRouter = require('./routes/user-router')
+const db = require("./db/connection");
+const userRouter = require("./routes/user-router");
 
-const app = express()
-const apiPort = 3000
+const app = express();
+const apiPort = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(bodyParser.json());
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.use('/api', userRouter)
+app.use("/api", userRouter);
 
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+app.listen(process.env.PORT || apiPort, () =>
+  console.log(`Server running on port ${apiPort}`)
+);
