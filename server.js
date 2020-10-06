9aa1a3286996a29a6e8aeb9096f76aa1af4829ff
Joshua Hart <joshua.hart@protonmail.com>
2020-10-06 16:45:13 +0100

    setting up to push to heroku

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const userRouter = require('./routes/user-router')
 
 const db = require("./db/connection");
+
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
@@ -11,10 +14,12 @@ app.use(bodyParser.json());
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World!!!!!!!!");
 });
 
 app.use("/api", userRouter);
+
+const apiPort = 3000;
 
 app.listen(process.env.PORT || apiPort, () =>
   console.log(`Server running on port ${apiPort}`)
