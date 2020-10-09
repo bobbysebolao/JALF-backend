de1fb957393b0ce55ca7b658789532977b851f5b
Bobby <bobbysebolao@gmail.com>
2020-10-09 12:39:45 +0100

    Add CORS middleware

--- server.js
+++ server.js
@@ -11,7 +11,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(bodyParser.json());
 
-app.options('*', cors())
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  next();
+}
+
+app.options(allowCrossDomain)
 
 // app.use((req, res, next) => {
 //   res.header('Access-Control-Allow-Origin', '*')
