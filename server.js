e521567144fb356138205a981c6fbd0d72db0cf1
Amber <a.rignell16@ejm.org>
2020-10-09 12:16:58 +0100

    Add control allow methods to server
    Co-authored-by: Josh <joshua.hart@protonmail.com>

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Method', 'PUT, GET, POST, OPTIONS');
   next();
 });
 
