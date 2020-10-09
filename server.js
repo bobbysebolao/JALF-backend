964d77f56d5a8efdd3ae39f114557c865f4c3d84
Bobby <bobbysebolao@gmail.com>
2020-10-09 13:32:50 +0100

    Tweak 5

--- server.js
+++ server.js
@@ -8,26 +8,10 @@ const db = require("./db/connection");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
 app.use(bodyParser.json());
 
-app.options('/api/store-data', cors());
-
-// var allowCrossDomain = function(req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type');
-
-//   next();
-// }
-
-// app.use(allowCrossDomain)
-
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*')
-//   res.header('Access-Control-Allow-Method', 'PUT, GET, POST, OPTIONS');
-//   next();
-// });
+app.use(cors());
+// app.options('/api/*', cors());
 
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
