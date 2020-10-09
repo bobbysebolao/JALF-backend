a2515b832e35576bf14a10a11fee38f37df24e77
Bobby <bobbysebolao@gmail.com>
2020-10-09 13:12:46 +0100

    Tweak

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var allowCrossDomain = function(req, res, next) {
   next();
 }
 
-app.options(allowCrossDomain)
+app.use(allowCrossDomain)
 
 // app.use((req, res, next) => {
 //   res.header('Access-Control-Allow-Origin', '*')
